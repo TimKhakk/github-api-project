268932363ede423abdfb175849bdf7ff70127f14
Timur Khakkiev <khakkievtimur@yandex.ru>
2022-09-04 09:16:05 +0300

    add search feature

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,51 @@
-import { useSearchUsersQuery } from "../store/github/github.api";
+import { useEffect, useState } from 'react';
+import { useDebounce } from '../hooks/useDebounce';
+import { useSearchUsersQuery } from '../store/github/github.api';
 
 export function HomePage() {
-  const { isLoading, isError, data } = useSearchUsersQuery('Tim');
-  return <div>HomePage</div>;
+  const [search, setSearch] = useState('');
+  const [dropdown, setDropdown] = useState(false);
+  const debounced = useDebounce(search);
+  const {
+    isLoading,
+    isError,
+    data: users,
+  } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3,
+    refetchOnFocus: true,
+  });
+
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && users?.length! > 0)
+  }, [debounced, users]);
+
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      {isError && <p className="text-center text-red-600">Something went wrong!</p>}
+
+      <div className="relative w-[560px]">
+        <input
+          type="text"
+          className="border py-2 px-4 w-full h-11 mb-2"
+          placeholder="Search for GitHub username..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        {dropdown && (
+          <ul className="list-none absolute top-11 overflow-y-scroll left-0 right-0 max-h-52 shadow-md bg-white">
+            {isLoading && <p className="">Loading...</p>}
+            {users?.map((user) => (
+              <li
+                className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+                key={user.id}
+              >
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
