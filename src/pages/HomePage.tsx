cda8278d80d0fff219524e4405118a3625b2125b
Timur Khakkiev <khakkievtimur@yandex.ru>
2022-09-04 08:47:47 +0300

    clean code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,5 @@ import { useSearchUsersQuery } from "../store/github/github.api";
 
 export function HomePage() {
   const { isLoading, isError, data } = useSearchUsersQuery('Tim');
-  console.log('🚀 ~ data', data);
   return <div>HomePage</div>;
 }
